02674d132527a753617abb5d58f0366262f40fbe
Yxl-l <18772988670@163.com>
2025-06-06 20:13:06 +0800

    Add files via upload

---
+++ day01-helloWord-app/src/com/yxl/helloword/HelloWord.java
@@ -0,0 +1,9 @@
+package com.yxl.helloword;
+
+public class HelloWord {
+    //    main+回车 生成main方法
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+
+    }
+}
