7d179d8656b7f12247c61d66269cb2734573c23d
Yxl-l <18772988670@163.com>
2025-06-06 20:13:52 +0800

    Delete day01-helloWord-app directory

--- day01-helloWord-app/src/com/yxl/helloword/HelloWord.java
+++
@@ -1,9 +0,0 @@
-package com.yxl.helloword;
-
-public class HelloWord {
-    //    main+回车 生成main方法
-    public static void main(String[] args) {
-        System.out.println("Hello Word");
-
-    }
-}
