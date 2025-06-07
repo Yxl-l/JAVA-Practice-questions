7a11f1a042f049956e54bb1cf9534f2052355fb6
杨小龙 <18772988670@163.com>
2025-06-07 23:51:06 +0800

    1

---
+++ JavaSE_Advanced/day09_Multi-threading/src/com/yxl/homework/demo01/Text03.java
@@ -0,0 +1,13 @@
+package com.yxl.homework.demo01;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class Text03 {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        FutureTask<Integer> f1 = new FutureTask<Integer>(new MyCallable(10));
+        Thread t1 = new Thread(f1);
+        t1.start();
+        System.out.println(f1.get());
+    }
+}
