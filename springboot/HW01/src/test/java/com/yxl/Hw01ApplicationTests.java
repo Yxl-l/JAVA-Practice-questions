4f6a1285e75dd90d54bfed9ca3627e636974bc32
杨小龙 <18772988670@163.com>
2025-06-24 21:35:57 +0800

    1

---
+++ springboot/HW01/src/test/java/com/yxl/Hw01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yxl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
