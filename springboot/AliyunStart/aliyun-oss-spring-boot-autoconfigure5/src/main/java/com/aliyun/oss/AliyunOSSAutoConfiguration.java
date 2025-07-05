7cdd629237f83600b4bb64ebb1380ad6dc788c51
杨小龙 <18772988670@163.com>
2025-07-05 11:23:28 +0800

    1

---
+++ springboot/AliyunStart/aliyun-oss-spring-boot-autoconfigure5/src/main/java/com/aliyun/oss/AliyunOSSAutoConfiguration.java
@@ -0,0 +1,14 @@
+package com.aliyun.oss;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(value = AliyunOSSProperties.class)
+public class AliyunOSSAutoConfiguration {
+    @Bean
+    public AliyunOSSOperator aliyunOSSOperator(AliyunOSSProperties aliyunOSSProperties){
+        return new AliyunOSSOperator(aliyunOSSProperties);
+    }
+}
